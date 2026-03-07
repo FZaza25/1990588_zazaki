e652c7d27aef0ea9b89080d16fdff218ab7be3cf
APulsoni <a.pulsoni@a-ble.com>
2026-03-07 13:06:33 +0100

    added login page

--- source/frontend/src/main.js
+++ source/frontend/src/main.js
@@ -1,13 +1,30 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router' // importa il router
+import './assets/main.scss'
 
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
-const vuetify = createVuetify({ components, directives })
+const vuetify = createVuetify({
+    components,
+    directives,
+    theme: {
+        defaultTheme: 'light',
+        themes: {
+            light: {
+                colors: {
+                    background: '#ffffff',
+                    border: '#0B304F',
+                    primary: '#0B304F'
+                },
+            },
+        },
+    },
+})
+
 
 createApp(App)
     .use(router)   // registra il router
