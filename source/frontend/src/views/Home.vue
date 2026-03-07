e56f9a4b9aef2bc2f22c17122012672f740e2e6c
APulsoni <a.pulsoni@a-ble.com>
2026-03-07 17:30:28 +0100

    fix login style

--- source/frontend/src/views/Home.vue
+++ source/frontend/src/views/Home.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="d-flex w-100 h-100 justify-center align-center">
-    <div class="login-card w-md-50 w-66 w-lg-25 h-50 rounded-lg border-md" >
+    <div class="login-card w-md-50 w-66 w-lg-25 h-66 rounded-lg border-md" >
       <div class="d-flex flex-column">
-          <img class="login-logo pt-8" src="/mars-iot-logo.png" alt="logo">
-          <div class="d-flex justify-center text-center w-100 font-weight-bold px-12 py-8" style="font-size: 20px">
+        <div class="login-logo-wrap">
+          <img class="login-logo" src="/mars-iot-logo.png" alt="logo">
+        </div>
+
+          <div class="d-flex justify-center text-center w-100 font-weight-bold text-background px-12 py-8" style="font-size: 20px">
             {{ t('login.subtitle') }}
           </div>
           <div class="d-flex justify-center py-8">
             <v-btn
-                class="rounded-lg w-33 bg-primary"
+                class="rounded-lg w-33 bg-background"
                 variant="text"
                 @click="router.push({ name: 'monitoring' })"
             >
