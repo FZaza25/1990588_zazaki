6ddb681bf7eb5918ceeee29b17bc2d064286a2a5
APulsoni <a.pulsoni@a-ble.com>
2026-03-08 19:50:58 +0100

    added area charts

---
+++ source/frontend/src/components/charts/AreaChart.vue
@@ -0,0 +1,89 @@
+<template>
+  <div style="height: 300px;">
+    <Line :data="data" :options="options" />
+  </div>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import { Line } from 'vue-chartjs'
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  LineElement,
+  PointElement,
+  CategoryScale,
+  LinearScale,
+  Filler
+} from 'chart.js'
+
+ChartJS.register(
+    Title,
+    Tooltip,
+    Legend,
+    LineElement,
+    PointElement,
+    CategoryScale,
+    LinearScale,
+    Filler
+)
+
+const props = defineProps({
+  labels: { type: Array, default: () => [] },
+  values: { type: Array, default: () => [] },
+  datasetLabel: { type: String, default: 'Telemetry' },
+  yUnit: { type: String, default: '' },
+  xUnit: { type: String, default: '' },
+  borderColor: { type: String, default: '#0B304F' },
+  fillColor: { type: String, default: 'rgba(11, 48, 79, 0.22)' }
+})
+
+const data = computed(() => ({
+  labels: [...props.labels],
+  datasets: [
+    {
+      label: props.datasetLabel,
+      data: [...props.values],
+      borderColor: props.borderColor,
+      backgroundColor: props.fillColor,
+      fill: true,
+      tension: 0.35,
+      pointRadius: 2,
+      pointHoverRadius: 4
+    }
+  ]
+}))
+
+const options = computed(() => ({
+  responsive: true,
+  maintainAspectRatio: false,
+  plugins: {
+    legend: { display: false },
+    tooltip: {
+      callbacks: {
+        label: (ctx) => {
+          const v = ctx.parsed.y
+          return `${v}${props.yUnit ? ` ${props.yUnit}` : ''}`
+        }
+      }
+    }
+  },
+  scales: {
+    x: {
+      ticks: {
+        callback: (value, index) => {
+          const l = props.labels[index]
+          return `${l}${props.xUnit ? ` ${props.xUnit}` : ''}`
+        }
+      }
+    },
+    y: {
+      ticks: {
+        callback: (v) => `${v}${props.yUnit ? ` ${props.yUnit}` : ''}`
+      }
+    }
+  }
+}))
+</script>
--- source/frontend/src/components/indoor_environment/Co2Hall.vue
+++ source/frontend/src/components/indoor_environment/Co2Hall.vue
@@ -1,12 +1,39 @@
 <template>
-  <UtilityCard :title="entries.sensor_id" :icon="entries.icon" :value="entries.value + ' ' + entries.unit"/>
+  <UtilityCard :title="entries.sensor_id" :icon="entries.icon" :value="entries.value + ' ' + entries.unit" :openChart="()=>openChart()"/>
+  <CentralModal ref="openModal">
+    <template #header>
+      <h1>
+        {{t('sensors_name.'+entries.sensor_id)}}
+      </h1>
+    </template>
+    <template #content>
+      <AreaChart :labels="chartTime" :values="sensorsData.charts.co2_hall" :yUnit="entries.unit"/>
+    </template>
+  </CentralModal>
 </template>
 
 <script setup>
 
 import UtilityCard from "../common/UtilityCard.vue";
+import {chartTime} from "../../data/ChartFunction.js";
+import CentralModal from "../common/CentralModal.vue";
+import {ref} from "vue";
+import {useSensorsStore} from "../../stores/sensors.js";
+import {useI18n} from "vue-i18n";
+import AreaChart from "../charts/AreaChart.vue";
+
+const openModal = ref(null)
+
+const sensorsData = useSensorsStore()
+
+const {t} = useI18n();
 
 const props = defineProps({
   entries: Object
 })
+
+const openChart = ()=>{
+  openModal.value.open()
+}
+
 </script>
