c7a66c9afc1ba0c22f8ddb786089c589ff5fee46
APulsoni <a.pulsoni@a-ble.com>
2026-03-08 14:26:33 +0100

    added sensors polling in WaterSystem

--- source/frontend/src/router/App/monitoring/WaterSystem.js
+++ source/frontend/src/router/App/monitoring/WaterSystem.js
@@ -2,6 +2,6 @@ export default {
     path: 'water-system',
     name: 'water_system',
     components: {
-
+        sensors: () => import('../../../views/monitoring/water_system/Sensors.vue'),
     }
 }
---
+++ source/frontend/src/views/monitoring/water_system/Sensors.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="title">
+    <h2>{{ t('sensors') }}</h2>
+    <div class="d-flex flex-column">
+      <div class="d-flex ga-4">
+        <HydroponicPH :entries="sensorsData?.hydroponic_ph" />
+        <WaterTankLevel :entries="sensorsData?.water_tank_level"/>
+      </div>
+    </div>
+
+
+  </div>
+</template>
+
+<script setup>
+
+import {useI18n} from "vue-i18n";
+import {onMounted,onUnmounted, ref} from "vue";
+import { api } from '../../../api/Request.js'
+import HydroponicPH from "../../../components/water_systems/HydroponicPH.vue";
+import WaterTankLevel from "../../../components/water_systems/WaterTankLevel.vue";
+
+const sensorsData = ref({
+  hydroponic_ph: {
+    icon: 'mdi-ph'
+  },
+  water_tank_level: {
+    icon: 'mdi-chart-waterfall'
+  }
+})
+
+const {t} = useI18n();
+
+let intervalId
+
+const handleData = (data) => {
+  data.map(sensor => {
+    if(sensor.sensor_id in sensorsData.value){
+      sensorsData.value[sensor.sensor_id] = {
+        ...sensor,
+        icon: sensorsData.value[sensor.sensor_id].icon,
+      };
+    }
+  })
+  console.log(sensorsData.value)
+}
+
+const get = async () => {
+  try {
+    const response = await api.get('/api/state')
+    handleData(response)
+  }catch (error) {
+    console.error(error)
+  }
+}
+
+onMounted(async () => {
+  await get()
+
+  intervalId = setInterval(async () => {
+    await get()
+  }, 5000)
+
+})
+
+onUnmounted(() => {
+  clearInterval(intervalId)
+})
+
+
+</script>
