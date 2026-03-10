1750bf0086ba75de26d885be7b376ddc20428927
APulsoni <a.pulsoni@a-ble.com>
2026-03-10 02:02:16 +0100

    added tools

--- source/frontend/src/components/actuators/ActuatorsTable.vue
+++ source/frontend/src/components/actuators/ActuatorsTable.vue
@@ -11,6 +11,7 @@
             hide-details
         />
         <v-switch
+            v-if="!auto"
             v-model="isOn"
             :label="isOn? 'ON':'OFF'"
             color="orange"
@@ -18,7 +19,7 @@
             hide-details/>
         <v-tooltip text="Add rule">
           <template v-slot:activator="{ props }">
-            <v-icon icon="mdi-plus" v-bind="props" size="32"/>
+            <v-icon icon="mdi-plus" class="cursor-pointer" v-bind="props" @click="()=>openAddModal()" size="32"/>
           </template>
         </v-tooltip>
 
@@ -29,20 +30,67 @@
         :headers="props.header"
         :items="props.items"
         class="actuators-data-table rounded-lg"
-    />
+    >
+      <template #item.actions="{ item }">
+        <div class="d-flex w-100 ga-4">
+          <v-tooltip text="Edit rule">
+            <template v-slot:activator="{ props }">
+              <v-icon icon="mdi-pencil" class="cursor-pointer" v-bind="props" size="32"/>
+            </template>
+          </v-tooltip>
+          <v-tooltip text="Delete rule">
+            <template v-slot:activator="{ props }">
+              <v-icon icon="mdi-trash-can-outline" class="cursor-pointer" v-bind="props" size="32"/>
+            </template>
+          </v-tooltip>
+        </div>
+      </template>
+    </v-data-table>
   </div>
-
+  <CentralModal ref="openModal">
+    <template #header>
+      <h1>Add a new rule for {{ t('actuators.'+props?.items[0]?.actuator_name) }}</h1>
+    </template>
+    <template #footer>
+      <div class="d-flex w-auto ga-4 justify-end">
+        <v-btn
+            class="rounded-lg bg-primary"
+            variant="text"
+            @click="()=>openModal.close()"
+        >
+          <div class="font-weight-bold">
+            Exit
+          </div>
+        </v-btn>
+        <v-btn
+            class="rounded-lg bg-primary"
+            variant="text"
+        >
+          <div class="font-weight-bold">
+            Confirm
+          </div>
+        </v-btn>
+      </div>
+    </template>
+  </CentralModal>
 </template>
 
 <script setup>
 import {useI18n} from "vue-i18n";
 import {ref} from "vue";
+import CentralModal from "../common/CentralModal.vue";
 
 const {t} = useI18n();
 
 const auto = ref(false)
 
 const isOn = ref(false);
+
+const openModal = ref(null);
+
+function openAddModal(){
+  openModal.value.open()
+}
 
 const props = defineProps({
   header: {
